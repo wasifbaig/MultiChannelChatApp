162485fd5df72800bb7fa6c0b6fc99ccb6714be4
Mirza Wasif Baig <wasif_baig@hotmail.com>
2016-12-15 03:25:20 +0100

    remove errors

--- db_mongo.js
+++
@@ -1,7 +0,0 @@
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
-var url_db = 'mongodb://localhost:27017/test';
-
-module.exports = function(callback) {
-    MongoClient.connect(url_db, callback);
-};
